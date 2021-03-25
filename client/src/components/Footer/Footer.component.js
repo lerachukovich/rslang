ef73ef064fbbf49d1dfe06cbf3585a31eca6044b
Lera Chukovich <chukalera@gmail.com>
2021-03-26 00:31:17 +0100

    feat: add promo page

--- client/src/components/Footer/Footer.component.js
+++ client/src/components/Footer/Footer.component.js
@@ -5,15 +5,19 @@ const Footer = () => {
     return (
         <footer className="page-footer">
             <div className="footer-container">
-                <a href="https://rs.school/js/"><img className='footer-logo'
-                                                     src="https://rs.school/images/rs_school_js.svg" alt="logo"/></a>
+                <a href="https://rs.school/js/">
+                    <img className='footer-logo' src="https://rs.school/images/rs_school_js.svg" alt="logo"/>
+                </a>
                 <div>2021</div>
-
                 <ul className='dev-links'>
-                    <li><a className="grey-text text-lighten-3" href="https://github.com/OlgaFedorovich">Alex</a></li>
-                    <li><a className="grey-text text-lighten-3" href="https://github.com/OlgaFedorovich">Olga</a></li>
-                    <li><a className="grey-text text-lighten-3" href="https://github.com/OlegMikhailov23">Oleg</a></li>
-                    <li><a className="grey-text text-lighten-3" href="https://github.com/lerachukovich">Lera</a></li>
+                    <li><a className="grey-text text-lighten-3" href="https://github.com/Alexus-bat"
+                           target='_blank' rel="noreferrer">Alex</a></li>
+                    <li><a className="grey-text text-lighten-3" href="https://github.com/OlgaFedorovich"
+                           target='_blank' rel="noreferrer">Olga</a></li>
+                    <li><a className="grey-text text-lighten-3" href="https://github.com/OlegMikhailov23"
+                           target='_blank' rel="noreferrer">Oleg</a></li>
+                    <li><a className="grey-text text-lighten-3" href="https://github.com/lerachukovich"
+                           target='_blank' rel="noreferrer">Lera</a></li>
                 </ul>
             </div>
         </footer>
