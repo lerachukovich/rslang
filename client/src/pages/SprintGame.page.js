d9185f3341cc712e890c5d3a116389fcebd30ba6
Lera Chukovich <chukalera@gmail.com>
2021-03-29 13:04:10 +0200

    feat: add Sprint game page

---
+++ client/src/pages/SprintGame.page.js
@@ -0,0 +1,10 @@
+import React from "react";
+import GameField from "../components/SprintGameComponents/GameField.component";
+
+const SprintGame = () => {
+    return(
+        <GameField />
+    )
+}
+
+export default SprintGame;
