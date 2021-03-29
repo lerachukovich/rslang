f48840b3c31727f327a594b17dce04de19578640
Lera Chukovich <chukalera@gmail.com>
2021-03-29 19:36:20 +0200

    feat: add Games page

--- client/src/pages/Crefdits.page.js
+++ client/src/pages/Crefdits.page.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import '../styles/Credits.page.scss';
-import DeveloperCard from "../components/CreditsPageComponents/Card.component";
+import lera from '../assets/lera.png';
+import alex from '../assets/alex.png';
+import olga from '../assets/olga.png';
+import oleg from '../assets/oleg.png';
 
 const CreditsPage = () => {
+    const developers = [
+        {
+            name: 'Алексей',
+            text: 'Бэкэнд, игра Аудиовызов, авторизация',
+            img_source: alex
+        },
+        {
+            name: 'Лера',
+            text: 'Игра Спринт, настройка первоначального стэйта проекта, дизайн',
+            img_source: lera
+        },
+        {
+            name: 'Олег',
+            text: 'Роутинг, Игра Саванна',
+            img_source: oleg
+        },
+        {
+            name: 'Ольга',
+            text: 'Учебник, своя игра',
+            img_source: olga
+        }
+    ]
   return(
     <div className='developers'>
       <h1 className='dev-title'>Наши разработчики</h1>
-        <DeveloperCard />
+        <div className='dev-container'>
+            {
+                developers.map(dev => {
+                    return <div className='dev-card'>
+                        <img className='dev-img' src={dev.img_source} alt="developer"/>
+                        <h3>{dev.name}</h3>
+                        <p>{dev.text}</p>
+                    </div>
+                })
+            }
+        </div>
     </div>
   )
 }
