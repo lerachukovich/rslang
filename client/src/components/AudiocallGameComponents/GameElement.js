56ef1d4165adaeff19386917d1fb1d05fbcaf55f
Alexus-bat <alexey.bond97@gmail.com>
2021-04-04 08:08:18 +0300

    fix: update statistic function

--- client/src/components/AudiocallGameComponents/GameElement.js
+++ client/src/components/AudiocallGameComponents/GameElement.js
@@ -10,7 +10,6 @@ const GameElement = ({sample, level, handleClick, readyNext}) => {
     const [isPlaySoundSecond, setIsPlaySoundSecond] = useState(false)
 
     useEffect(() => {
-        console.log(answer.wordTranslate)
         playSound(answer.audio);
         setSampleWords(shuffle(sample));
     }, [sample])
