52f05d9311d5d4610d3d58b04e6043176728673e
Oleg Mikhaylov <dkfolkin@gmail.com>
2021-04-12 00:45:00 +0300

    feat: creating vocabulary(in progress)

---
+++ client/src/helper/database.helper/updateUserWord.helper.js
@@ -0,0 +1,14 @@
+export const UpdateUserWord = async ({ userId, wordId, word }, token) => {
+  const rawResponse = await fetch(`/users/${userId}/words/${wordId}`, {
+    method: 'PUT',
+    withCredentials: true,
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(word)
+  });
+  const content = await rawResponse.json();
+};
+
