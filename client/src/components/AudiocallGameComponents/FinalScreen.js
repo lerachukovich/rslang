4b5e73808f5ba7ea3b651cdbbc4565b6a50c745d
Oleg Mikhaylov <dkfolkin@gmail.com>
2021-04-14 18:42:46 +0300

    fix: fixed result in Audiocall-game

--- client/src/components/AudiocallGameComponents/FinalScreen.js
+++ client/src/components/AudiocallGameComponents/FinalScreen.js
@@ -1,34 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { SoundHandler } from '../../helper/Sound.helper';
 
 const FinalScreen = ({ value }) => {
-
-    const playSound = url => {
-        const audio = new Audio(`/${url}`);
-        audio.play();
-    }
+    const {answers} = value;
+    useEffect(() => {
+        console.log(value.isFromTextBook)
+    }, []);
 
     return (
         <div className={'savanna__final-screen-wrapper'}>
         <h2 className={'savanna__final-screen__title'}>Статистика игры</h2>
-        <b>Я знаю <span className={'savanna__final-screen__count correct'}>{value.correct.length}</span></b>
+        <b>Я знаю <span className={'savanna__final-screen__count correct'}>{answers.correct.length}</span></b>
         <ul>
-            {value.correct.length === 0 &&
+            {answers.correct.length === 0 &&
             <p className={'savanna_final-screen__quote'}>Блин, я ничего не знаю :(</p>}
-            {value.correct.map((it, ind) => (
+            {answers.correct.map((it, ind) => (
             <li className={'savanna-final-screen__word'} key={ind}>
-                <button className={'btn'} onClick={() => playSound(it.audio)}><i className={'material-icons'}>surround_sound</i></button>
+                <button className={'btn'} onClick={() => SoundHandler(it.audio)}><i className={'material-icons'}>surround_sound</i></button>
                 {it.word} - {it.wordTranslate}</li>
             ))}
         </ul>
         <b>Я не знаю <span
-            className={'savanna__final-screen__count uncorrect'}>{value.unCorrect.length}</span></b>
+            className={'savanna__final-screen__count uncorrect'}>{answers.unCorrect.length}</span></b>
         <ul>
-            {value.correct.length === 10 && <p>Ох, какой я красавчик :)</p>}
-            {value.unCorrect.map((it, ind) => (
+            {answers.correct.length === 10 && <p>Ох, какой я красавчик :)</p>}
+            {answers.unCorrect.map((it, ind) => (
             <li className={'savanna-final-screen__word'} key={ind}>
-                <button className={'btn'} onClick={() => playSound(it.audio)}><i className={'material-icons'}>surround_sound</i></button>
-                {it.word} - {it.wordTranslate}</li>
+                <button className={'btn'} onClick={() => SoundHandler(it.audio)}><i className={'material-icons'}>surround_sound</i></button>
+                {it.word} - {it.wordTranslate}
+                {value.isFromTextBook && <button className={'savanna__final-screen__word-list__btn--gohard'} onClick={value.setHardDif}
+                         wordid={it.id} title={'Переместить в сложные'}>Сложное слово</button>}
+            </li>
             ))}
         </ul>
         <Link to="/games" className={'savanna__final-screen__next-button btn'}>Продолжить</Link>
